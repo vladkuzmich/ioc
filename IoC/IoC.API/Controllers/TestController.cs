a249daae3f180594a01cf3d858a03a4b31dee684
kuzmichvs <vladislav.kuzmich@oxagile.com>
2019-11-12 15:36:42 +0300

    SBX-817: Added BaseController

--- IoC/IoC.API/Controllers/TestController.cs
+++ IoC/IoC.API/Controllers/TestController.cs
@@ -1,31 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using IoC.Business.Contracts;
 using Microsoft.Extensions.Logging;
+using IoC.Business.Contracts;
 
 namespace IoC.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TestController : ControllerBase
+    public class TestController : BaseController
     {
-        private readonly ITestService _testService;
-        private readonly ILogger<TestController> _logger;
-
         public TestController(
             ITestService testService,
             ILogger<TestController> logger)
-        {
-            _testService = testService ?? throw new ArgumentNullException(nameof(testService));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        }
+            : base(testService, logger)
+        { }
 
         // GET api/test
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            _logger.LogInformation("Test endpoint was called");
+            Logger.LogInformation("Test endpoint was called");
 
             return new[] { "value1", "value2" };
         } 
